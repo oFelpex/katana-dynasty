a3331f5177153475ef11d7c28d20d005ec75193c
Felipe Costa da Silva <felipe95176@gmail.com>
2024-11-19 11:08:18 -0300

    Add a footer, but just for tests. Add the header content and the button that may scroll to catalog (in progress).

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { CherryBlossomPetalsComponent } from './cherry-blossom-petals/cherry-blossom-petals.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NavBarComponent, CherryBlossomPetalsComponent],
+  imports: [
+    NavBarComponent,
+    CherryBlossomPetalsComponent,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  wallpaperNumber: number;
+  wallpaper: string;
+  constructor() {
+    this.wallpaperNumber = Number((Math.random() * 5).toFixed(0));
+    this.wallpaper = `../../../assets/images/backgrounds/background-${this.wallpaperNumber}.webp`;
+  }
+
+  moveDown(): void {
+    const target = document.getElementById('test');
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+}
