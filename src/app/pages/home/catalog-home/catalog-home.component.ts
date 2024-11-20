a0181dd02907277082c565707d3fc5bd87019660
Felipe Costa <felipe95176@gmail.com>
2024-11-20 19:26:45 -0300

    Chance the classes of the card, in accord of the type of katana (commun, legendary...).

--- src/app/pages/home/catalog-home/catalog-home.component.ts
+++ src/app/pages/home/catalog-home/catalog-home.component.ts
@@ -1,44 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 import { CatalogCardComponent } from './catalog-card/catalog-card.component';
 import { CommunKatanas } from '../../../models/commun-katanas';
 import { LegendaryKatanas } from '../../../models/legendary-katanas';
 import { MagicKatanas } from '../../../models/magic-katanas';
+import { CommunKatanasService } from '../../../services/commun-katanas/commun-katanas.service';
+import { LegendaryKatanasService } from '../../../services/legendary-katanas/legendary-katanas.service';
+import { MagicKatanasService } from '../../../services/magic-katanas/magic-katanas.service';
+import { Katanas } from '../../../models/katanas';
 
 @Component({
   selector: 'app-catalog-home',
   standalone: true,
-  imports: [CatalogCardComponent],
+  imports: [CatalogCardComponent, CommonModule],
   templateUrl: './catalog-home.component.html',
   styleUrl: './catalog-home.component.scss',
 })
-export class CatalogHomeComponent {
-  newlyAddedKatanas: CommunKatanas[] | LegendaryKatanas[] | MagicKatanas[] = [
-    {
-      id: 0,
-      imgSRC:
-        '../../../../assets/images/products/katanas/communKatanas/Great Katana.webp',
-      imgALT: 'A Great Katana',
-      title: 'Great Katana',
-      description: `Large katana with a long, heavy blade. A weapon unique to warriors of the Land of Reeds. Designed for aggression, this armament requires the wielder to throw their entire body into swinging it. The slashing attacks of its honed edge incite blood loss.`,
-      price: 1200,
-    },
-    {
-      id: 1,
-      imgSRC:
-        '../../../../assets/images/products/katanas/communKatanas/Nagakiba.webp',
-      imgALT: 'A Long Katana',
-      title: 'Nagakiba',
-      description: `Katana with a ferociously long blade. Signature weapon of Yura, hunter of Bloody Fingers. Reminiscent of a reinforced spear, its imposing length can be put to good use with powerful thrusting attacks.`,
-      price: 1700,
-    },
-    {
-      id: 2,
-      imgSRC:
-        '../../../../assets/images/products/katanas/communKatanas/Wado Ichimonji.webp',
-      imgALT: 'Wado Ichimonji',
-      title: 'Wado Ichimonji',
-      description: `The Wado Ichimonji is a sword of great personal importance to Roronoa Zoro. It was forged by Shimotsuki Kouzaburou and was once a prized heirloom of his family. It is also one of the 21 Great Grade swords. After the death of its previous owner, Kuina, Zoro asked for it from her father, who then gave the sword to him.`,
-      price: 950,
-    },
-  ];
+export class CatalogHomeComponent implements OnInit {
+  communKatanas: CommunKatanas[] = [];
+  legendaryKatanas: LegendaryKatanas[] = [];
+  magicKatanas: MagicKatanas[] = [];
+  newlyAddedKatanas: Katanas[] = [];
+
+  constructor(
+    private communKatanasService: CommunKatanasService,
+    private legendaryKatanasService: LegendaryKatanasService,
+    private magicKatanasService: MagicKatanasService
+  ) {}
+
+  ngOnInit(): void {
+    this.communKatanas = this.communKatanasService.getCommunKatanas();
+    this.legendaryKatanas = this.legendaryKatanasService.getLegendaryKatanas();
+    this.magicKatanas = this.magicKatanasService.getMagicKatanas();
+
+    this.newlyAddedKatanas.push(
+      this.communKatanas[2],
+      this.magicKatanas[0],
+      this.legendaryKatanas[0]
+    );
+  }
 }
