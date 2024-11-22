803cbdb0157bb0d3c02849b5d06d62667b5b5c32
Felipe Costa da Silva <felipe95176@gmail.com>
2024-11-22 12:27:23 -0300

    Add cherry blossom animation.

--- src/app/shared/header/cherry-blossom-petals/cherry-blossom-petals.component.ts
+++ src/app/shared/header/cherry-blossom-petals/cherry-blossom-petals.component.ts
@@ -1,9 +1,205 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  Renderer2,
+  ViewEncapsulation,
+} from '@angular/core';
+
+export type BlossomSceneConfig = {
+  id: string;
+  petalsTypes: Petal[];
+  numPetals?: number;
+  gravity?: number; // 0.2 ~ 1
+  windMaxSpeed?: number;
+};
+
+interface PetalConfig {
+  customClass?: string;
+  x?: number;
+  y?: number;
+  z?: number;
+  xSpeedVariation?: number;
+  ySpeed?: number;
+  rotation?: PetalRotation;
+}
+
+type PetalRotation = {
+  axis: 'X' | 'Y' | 'Z';
+  value: number;
+  speed: number;
+  x: number;
+};
+
+export class Petal implements PetalConfig {
+  el: HTMLElement;
+  customClass: string;
+  x: number;
+  y: number;
+  z: number;
+  xSpeedVariation: number;
+  ySpeed: number;
+  rotation: { axis: 'X' | 'Y' | 'Z'; value: number; speed: number; x: number };
+
+  constructor(config: PetalConfig) {
+    this.customClass = config.customClass || '';
+    this.x = config.x || 0;
+    this.y = config.y || 0;
+    this.z = config.z || 0;
+    this.xSpeedVariation = config.xSpeedVariation || 0;
+    this.ySpeed = config.ySpeed || 0;
+    this.rotation = {
+      axis: 'X',
+      value: 0,
+      speed: 0,
+      x: 0,
+    };
+
+    if (config.rotation) {
+      this.rotation.axis = config.rotation.axis || this.rotation.axis;
+      this.rotation.value = config.rotation.value || this.rotation.value;
+      this.rotation.speed = config.rotation.speed || this.rotation.speed;
+      this.rotation.x = config.rotation.x || this.rotation.x;
+    }
+
+    this.el = document.createElement('div');
+    this.el.className = `petal ${this.customClass}`;
+    this.el.style.position = 'absolute';
+    this.el.style.backfaceVisibility = 'visible';
+  }
+}
+
 @Component({
   selector: 'app-cherry-blossom-petals',
   standalone: true,
-  imports: [],
   templateUrl: './cherry-blossom-petals.component.html',
-  styleUrl: './cherry-blossom-petals.component.scss',
+  styleUrls: ['./cherry-blossom-petals.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class CherryBlossomPetalsComponent {}
+export class CherryBlossomPetalsComponent implements OnInit {
+  @Input() config!: BlossomSceneConfig;
+
+  private petals: Petal[] = [];
+  private width = 0;
+  private height = 0;
+  private windMagnitude = 0.2;
+  private windDuration = 0;
+  private timer = 0;
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  ngOnInit(): void {
+    this.validateConfig();
+
+    const container = this.el.nativeElement.querySelector(`#${this.config.id}`);
+    if (!container) {
+      throw new Error(`Container with ID "${this.config.id}" not found.`);
+    }
+
+    this.width = container.offsetWidth;
+    this.height = container.offsetHeight;
+
+    this.createPetals(container);
+    requestAnimationFrame(this.updateFrame.bind(this));
+  }
+
+  private validateConfig(): void {
+    if (!this.config) {
+      throw new Error('Configuration object (config) is required.');
+    }
+
+    if (!this.config.petalsTypes || this.config.petalsTypes.length === 0) {
+      throw new Error('At least one petal type is required in "petalsTypes".');
+    }
+  }
+
+  private createPetals(container: HTMLElement): void {
+    for (let i = 0; i < this.config.numPetals!; i++) {
+      const petalType =
+        this.config.petalsTypes[
+          Math.floor(Math.random() * this.config.petalsTypes.length)
+        ];
+      const petal = new Petal({ customClass: petalType.customClass });
+
+      this.resetPetal(petal);
+      this.petals.push(petal);
+      container.appendChild(petal.el);
+    }
+  }
+
+  private resetPetal(petal: Petal): void {
+    petal.x = this.width - Math.random() * this.width * 1.75;
+    petal.y = petal.el.offsetHeight - Math.random() * this.height;
+    petal.z = Math.random() * 200;
+
+    if (petal.x > this.width) {
+      petal.x = this.width + petal.el.offsetWidth;
+      petal.y = (Math.random() * this.height) / 2;
+    }
+
+    petal.rotation.speed = Math.random() * 10;
+    const randomAxis = Math.random();
+    if (randomAxis > 0.5) {
+      petal.rotation.axis = 'X';
+    } else if (randomAxis > 0.25) {
+      petal.rotation.axis = 'Y';
+      petal.rotation.x = Math.random() * 180 + 90;
+    } else {
+      petal.rotation.axis = 'Z';
+      petal.rotation.x = Math.random() * 360 - 180;
+      petal.rotation.speed = Math.random() * 3;
+    }
+
+    petal.xSpeedVariation = Math.random() * 0.8 - 0.4;
+    petal.ySpeed = Math.random() + this.config.gravity!;
+  }
+
+  private updateFrame(): void {
+    if (this.timer === this.windDuration) {
+      this.updateWind();
+      this.timer = 0;
+    }
+
+    for (const petal of this.petals) {
+      this.updatePetal(petal);
+    }
+
+    this.timer++;
+    requestAnimationFrame(this.updateFrame.bind(this));
+  }
+
+  private updatePetal(petal: Petal): void {
+    const windSpeed = this.calculateWindSpeed(this.timer, petal.y);
+    const xSpeed = windSpeed + petal.xSpeedVariation;
+
+    petal.x -= xSpeed;
+    petal.y += petal.ySpeed;
+    petal.rotation.value += petal.rotation.speed;
+
+    let transform = `translateX(${petal.x}px) translateY(${petal.y}px) translateZ(${petal.z}px) rotate${petal.rotation.axis}(${petal.rotation.value}deg)`;
+    if (petal.rotation.axis !== 'X') {
+      transform += ` rotateX(${petal.rotation.x}deg)`;
+    }
+    petal.el.style.transform = transform;
+
+    if (petal.x < -10 || petal.y > this.height + 10) {
+      this.resetPetal(petal);
+    }
+  }
+
+  private calculateWindSpeed(t: number, y: number): number {
+    const a =
+      (this.windMagnitude / 2) * ((this.height - (2 * y) / 3) / this.height);
+    return (
+      a *
+        Math.sin(((2 * Math.PI) / this.windDuration) * t + (3 * Math.PI) / 2) +
+      a
+    );
+  }
+
+  private updateWind(): void {
+    this.windMagnitude = Math.random() * this.config.windMaxSpeed!;
+    this.windDuration = this.windMagnitude * 50 + (Math.random() * 20 - 10);
+  }
+}
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,27 +1,35 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { FormsModule } from '@angular/forms';
+
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { CherryBlossomPetalsComponent } from './cherry-blossom-petals/cherry-blossom-petals.component';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-
+import {
+  BlossomSceneConfig,
+  Petal,
+} from './cherry-blossom-petals/cherry-blossom-petals.component';
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
     NavBarComponent,
     CherryBlossomPetalsComponent,
+    CommonModule,
     MatIconModule,
-    MatButtonModule,
+    MatSlideToggleModule,
+    FormsModule,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
   wallpaperNumber: number;
-  wallpaper: string;
+  wallpaperSRC: string;
   constructor() {
     this.wallpaperNumber = Number((Math.random() * 5).toFixed(0));
-    this.wallpaper = `../../../assets/images/backgrounds/background-${this.wallpaperNumber}.webp`;
+    this.wallpaperSRC = `../../../assets/images/backgrounds/background-${this.wallpaperNumber}.webp`;
   }
 
   moveDown(): void {
@@ -30,4 +38,18 @@ export class HeaderComponent {
       target.scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
   }
+
+  petalsEnabled: boolean = true;
+  blossomConfig: BlossomSceneConfig = {
+    id: 'blossom-container',
+    petalsTypes: [
+      new Petal({ customClass: 'petal-style1' }),
+      new Petal({ customClass: 'petal-style2' }),
+      new Petal({ customClass: 'petal-style3' }),
+      new Petal({ customClass: 'petal-style4' }),
+    ],
+    numPetals: Math.random() * 12,
+    gravity: Math.random() * (1 - 0.2) + 0.2,
+    windMaxSpeed: Math.random() * 5,
+  };
 }
