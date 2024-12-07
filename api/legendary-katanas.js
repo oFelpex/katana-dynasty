b338d36441adabd3018599b2d171c4d02d692f37
Felipe Costa <felipe95176@gmail.com>
2024-12-07 14:30:24 -0300

    ✨ Add vercel configuration

---
+++ api/legendary-katanas.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const path = require("path");
+
+export default function handler(req, res) {
+  const filePath = path.join(process.cwd(), "../db.json"); // Caminho para o db.json
+  const jsonData = fs.readFileSync(filePath, "utf8");
+  const data = JSON.parse(jsonData);
+
+  // Retorna apenas as katanas do db.json
+  res.status(200).json(data["legendary-katanas"]);
+}
